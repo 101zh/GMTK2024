b49253af495b3946aaf5699d1869cbd0d8b5f218
Dr. Jovo <64760590+JonathanGamin@users.noreply.github.com>
2024-08-17 19:05:57 -0700

    Added Basic Gun and Scalable Objects
    -- The gun functions, but its mode must be manually set --
    -- No visuals for anything --
    -- Objects can be scaled, but are not aligned to grid --

--- Assets/Scripts/Player/GunController.cs
+++ Assets/Scripts/Player/GunController.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class GunController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform shootOrigin;
+    public LayerMask scalableLayer;
+
+    // Mode 0 = Shrink
+    // Mode 1 = Normal
+    // Mode 2 = Grow
+    public int mode = 0;
+
+    bool isHovering = false;
+    ScalableObject selected = null;
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckIfSelectingBlock();
+
+        if (isHovering)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                selected.ChangeSize(mode);
+            }
+        }
+    }
+
+    void LookAtMouse()
+    {
+        var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        if (Mathf.Abs(angle) > 100)
+            transform.localScale = new Vector3(1, -1, 1);
+        else
+            transform.localScale = new Vector3(1, 1, 1);
+
+        Debug.Log("Looking");
+    }
+
+    void CheckIfSelectingBlock()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+        if (hit.collider != null)
+        {
+            isHovering = true;
+            selected = hit.collider.gameObject.GetComponent<ScalableObject>();
+            LookAtMouse();
+        }
+        else
+        {
+            isHovering = false;
+            selected = null;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            transform.localScale = new Vector3(1, 1, 1);
+        }
     }
 }
---
+++ Assets/Scripts/Scalable Objects/ScalableObject.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScalableObject : MonoBehaviour
+{
+    [Header("Scale")]
+    public float defaultScale = 1.0f;
+    public float bigScale = 1.5f;
+    public float smallScale = 0.5f;
+    int currentSize; // Int 0, 1, or 2
+    float prevScale = 1.0f; // Float equal to either smallScale, defaultScale, or bigScale
+    float currentScale = 1.0f; // Float equal to either smallScale, defaultScale, or bigScale
+
+    // Size 0 = Shrink
+    // Size 1 = Normal
+    // Size 2 = Grow
+    public void ChangeSize(int size)
+    {
+        switch (size)
+        {
+            case 0:
+                transform.localScale = Vector2.one * smallScale;
+                currentSize = 0;
+
+                prevScale = currentScale;
+                currentScale = smallScale;
+                break;
+            case 1:
+                transform.localScale = Vector2.one * defaultScale;
+                currentSize = 1;
+
+                prevScale = currentScale;
+                currentScale = defaultScale;
+                break;
+            case 2:
+                transform.localScale = Vector2.one * bigScale;
+                currentSize = 2;
+
+                prevScale = currentScale;
+                currentScale = bigScale;
+                break;
+        }
+
+        transform.Translate(Vector2.up * ((currentScale - prevScale) / 4f));
+    }
+}
